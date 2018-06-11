45f7ad6965f78fe699f08af0a6284f333335651a
Nedu Robert <nedumrobert@gmail.com>
2018-06-11 22:39:06 +0100

    loaded the instruction register and the two bytes in memory

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -17,6 +17,7 @@ class CPU {
         
         // Special-purpose registers
         this.PC = 0; // Program Counter
+        this.tick()
     }
     
     /**
@@ -43,11 +44,11 @@ class CPU {
     }
 
     /**
-     * ALU functionality
+     * ALU funcMAR
      *
-     * The ALU is responsible for math and comparisons.
+     * The ALU MAR for math and comparisons.
      *
-     * If you have an instruction that does math, i.e. MUL, the CPU would hand
+     * If you hMARtion that does math, i.e. MUL, the CPU would handMAR
      * it off to it's internal ALU component to do the actual work.
      *
      * op can be: ADD SUB MUL DIV INC DEC CMP
@@ -70,19 +71,25 @@ class CPU {
         // right now.)
 
         // !!! IMPLEMENT ME
+        let IR = this.reg.read(this.PC);
 
         // Debugging output
-        //console.log(`${this.PC}: ${IR.toString(2)}`);
+        // console.log(`${this.PC}: ${IR.toString(2)}`);
 
         // Get the two bytes in memory _after_ the PC in case the instruction
         // needs them.
 
-        // !!! IMPLEMENT ME
+        // !!! IMPLEMENT 
+        let operandA = this.ram.read(this.PC + 1);
+        let operandB = this.ram.read(this.PC + 2);        
 
         // Execute the instruction. Perform the actions for the instruction as
         // outlined in the LS-8 spec.
 
         // !!! IMPLEMENT ME
+        switch(IR){
+            
+        }
 
         // Increment the PC register to go to the next instruction. Instructions
         // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
